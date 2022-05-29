8d5d45affea754b0ac4cf2a077c36c59814057fb
Vladimir Danilin <36300059+Rus-amok@users.noreply.github.com>
2022-05-29 22:53:55 +0300

    added coins by api

---
+++ CryptoObserver/Models/Coin.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CryptoObserver.Models
+{
+    public class Coin: INotifyPropertyChanged
+    {
+        private string id;
+        private int rank;
+        private string symbol;
+        private string name;
+        private float priceUsd;
+
+        private List<string> markets;
+
+        public string Id
+        {
+            get { return id; }
+            set
+            {
+                id = value;
+                OnPropertyChanged("Id");
+            }
+        }
+        public int Rank
+        {
+            get { return rank; }
+            set
+            {
+                rank = value;
+                OnPropertyChanged("Rank");
+            }
+        }
+        public string Symbol
+        {
+            get { return symbol; }
+            set
+            {
+                symbol = value;
+                OnPropertyChanged("Symbol");
+            }
+        }
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                name = value;
+                OnPropertyChanged("Name");
+            }
+        }
+        public float PriceUsd
+        {
+            get { return priceUsd; }
+            set
+            {
+                priceUsd = value;
+                OnPropertyChanged("PriceUsd");
+            }
+        }
+        public List<string> Markets
+        {
+            get { return markets; }
+            set
+            {
+                markets = value;
+                OnPropertyChanged("Markets");
+            }
+        }
+     
+        public event PropertyChangedEventHandler PropertyChanged;              
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+
+
+    }
+
+
+}
---
+++ CryptoObserver/ViewModels/CoinViewModel.cs
@@ -0,0 +1,70 @@
+﻿using CryptoObserver.Models;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Runtime.CompilerServices;
+
+namespace CryptoObserver.ViewModels
+{
+    public class CoinViewModel
+    {
+        private ObservableCollection<Coin> coinsList;
+        public ObservableCollection<Coin> CoinsList { get { return this.coinsList; } }
+        public CoinViewModel()
+        {
+            this.coinsList = new ObservableCollection<Coin>();
+
+            HttpClient httpClient=new HttpClient();
+            DataSource dataSource = new DataSource(httpClient);
+            dataSource.LoadCoinsList(ref this.coinsList);
+        }
+    }
+
+    internal class DataSource
+    {
+        private readonly HttpClient _httpClient;
+
+        public DataSource(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<List<Coin>> GetCoinsListFromApiAsync(CancellationToken cancellationToken = default)
+        {
+            using var response = await _httpClient.GetAsync("http://api.coincap.io/v2/assets", cancellationToken);
+            var strResult = await response.Content.ReadAsStringAsync();
+            JObject coinData = JObject.Parse(strResult);
+
+            IList<JToken> results = coinData["data"].Children().ToList();
+            List<Coin> coins = new();
+
+            foreach (JToken res in results)
+            {
+                Coin coin = res.ToObject<Coin>();
+                coins.Add(coin);
+            }
+            coins.OrderBy(x => x.Rank);
+            return coins;
+        }
+
+        public void GetCoinsList(ref ObservableCollection<Coin> coinsList)
+        {
+            var tempCoinList = GetCoinsListFromApiAsync();
+            foreach (var coin in tempCoinList.Result)
+            {
+                coinsList.Add(coin);
+            }
+        }
+
+        public void LoadCoinsList(ref ObservableCollection<Coin> coinsList)
+        {
+            GetCoinsList(ref coinsList);
+        }
+    }
+}
